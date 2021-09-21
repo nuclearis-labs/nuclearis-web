cbf1c782f0d78231ca6374c5748db0ebfe8fc919
Pablo Garban <pgarban@nuclearis.com>
2021-09-21 12:54:13 -0300

    feat: actualizar socios

--- src/content/partners.js
+++ src/content/partners.js
@@ -2,17 +2,17 @@ const partnersContent = [
     {
         id: "partner_1",
         title: "BGH",
-        text: "Incorporated in 1969 as a one-man tool and die shop for the Ford Motor Company, Bogue Machine Company has gradually evolved into a state-of-the-art twenty-first-century machining facility that produces complex components for both civilian and defense contractors.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id: "partner_2",
         title: "Disegno AC",
-        text: "Continental Machining Company offers precision machining services. The Company provides solutions including design, prototype, welding, fabrication, CNC milling, and other related services. Continental Machining serves customers in the United States.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id: "partner_3",
         title: "IMECO",
-        text: "Continental Machining Company offers precision machining services. The Company provides solutions including design, prototype, welding, fabrication, CNC milling, and other related services. Continental Machining serves customers in the United States.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
   ]
   
