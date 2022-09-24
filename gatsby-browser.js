7caabe6b23173a1a31a1541fcda06d6aef9ebef5
Cristian Leonel Romero <c.leonel.romero@outlook.es>
2022-09-24 18:42:17 -0300

    Sitio anterior eliminado

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-import "@fontsource/ubuntu"
-import "@fontsource/ubuntu/300.css" // Weight 500 with all styles included.
-import "./src/css/index.css"
